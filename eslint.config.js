fffde47cc0fea12d4121d9d6d2f67a3a4094537c
dzx <dzxcode@gmail.com>
2026-03-16 23:27:10 +0800

    chore: 添加 Prettier 和 ESLint 工具链配置

---
+++ eslint.config.js
@@ -0,0 +1,24 @@
+import globals from 'globals'
+import pluginVue from 'eslint-plugin-vue'
+import tseslint from 'typescript-eslint'
+
+export default tseslint.config(
+  {
+    ignores: ['node_modules', 'dist', '.opencode', '.poc']
+  },
+  ...pluginVue.configs['flat/essential'],
+  {
+    languageOptions: {
+      ecmaVersion: 'latest',
+      globals: {
+        ...globals.browser,
+        ...globals.node
+      }
+    },
+    rules: {
+      'vue/multi-word-component-names': 'off',
+      'no-unused-vars': 'warn',
+      'no-console': 'warn'
+    }
+  }
+)
